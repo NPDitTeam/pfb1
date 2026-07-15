b306e46ef465da7697de9e19ec820ec05a84bbda
Manjulyy <apisit.chai.m@gmail.com>
2026-07-15 14:54:23 +0700

    Add pfb_npd_payment_form_receipt_place_rent module (rent-place receipt)
    Print button label set to 'ใบเสร็จรับเงินค่าเช่าที่'

---
+++ npd/pfb_npd_payment_form_receipt_place_rent/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'ใบเสร็จค่าเช่าสถานที่',
+    'version': '14.0.0.1',
+    'summary': 'Module for managing custom features',
+    'description': """Module for managing custom features""",
+    'author': 'Your Name',
+    'depends': ['base', 'payment'],
+    'data': [
+        'report/pfb_npd_payment_form.xml'
+    ],
+}
